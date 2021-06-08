c51c536298e0d25770f8a2e5158a02e62958d6c2
ThinkerPal <thinkerpal@gmail.com>
2021-06-08 16:47:43 +0800

    made colours a state variable

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
-const COLOURS = [
-  { red: 255, green: 0, blue: 0, alpha: 1, id: "0" },
-  { red: 0, green: 255, blue: 0, alpha: 1, id: "1" },
-  { red: 0, green: 0, blue: 255, alpha: 1, id: "2" },
-];
-
 function HomeScreen() {
+  const [colours, setColours] = useState([
+    { red: 255, green: 0, blue: 0, alpha: 1, id: "0" },
+    { red: 0, green: 255, blue: 0, alpha: 1, id: "1" },
+    { red: 0, green: 0, blue: 255, alpha: 1, id: "2" },
+  ]);
   function renderItem({ item }) {
     return (
       <BlockRGB
